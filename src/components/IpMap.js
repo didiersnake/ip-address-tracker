0f79b8dc86bf542dd9a4b67ae1f50174cdcfd833
didiersnake <didier.djakoua@gmail.com>
2023-04-19 22:54:31 +0100

    Location mmarker

--- src/components/IpMap.js
+++ src/components/IpMap.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
+import React, {useState} from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "./ipMap.css"
 import { Icon } from 'leaflet'
 
+
+const DEFAULT_LONGITUDE = 51.505;
+const DEFAULT_LATITUDE = -0.09;
 const IpMap = () => {
+
+    function LocationMarker() {
+        const [position, setPosition] = useState(null)
+        const map = useMapEvents({
+            click() {
+                map.locate()
+            },
+            locationfound(e) {
+                setPosition(e.latlng)
+                map.flyTo(e.latlng, map.getZoom())
+            },
+        })
+        return position === null ? null : (
+          <Marker position={position} icon={customIcon}>
+            <Popup>You are here</Popup>
+          </Marker>
+        );
+    }
 
     const customIcon = new Icon({
       iconUrl: require("../images/icon-location.svg"),
       iconSize: [30, 30], //size of icon
     });
+    
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[DEFAULT_LONGITUDE, DEFAULT_LATITUDE]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]} icon={customIcon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LocationMarker/>
     </MapContainer>
   );
 }
